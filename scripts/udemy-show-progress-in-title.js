47225826b091a7f76547dd35230d3a36068ee174
ArashRasteh <nowruzr@gmail.com>
2020-01-17 16:09:25 -0700

    Update Udemy-show-pr.. to add refreshing without refreshing webpage

--- scripts/udemy-show-progress-in-title.js
+++ scripts/udemy-show-progress-in-title.js
@@ -2,43 +2,55 @@
 // @name         Udemy: show progress of course in Title and after 'Your progress'
 // @namespace    http://tampermonkey.net/
 // @version      1.0
-// @description  try to take over the world!
+// @description  Also if you click the circle with the trophy inside, will update the progress (sometimes needs to be clicked twice);
 // @author       Arash Rasteh
 // @match        https://www.udemy.com/course/*/learn/lecture/*
 // @grant        none
 // ==/UserScript==
 
 const showProgressInTitle = () => {
-  let progressContainerLoadedAndClicked = false;
+  return new Promise((resolve, reject) => {
+    let progressContainerLoadedAndClicked = false;
+    let counter = 0;
 
-  const udemyStatInterval = setInterval(function(){
-
+    const udemyStatInterval = setInterval(function () {
+      counter++;
+      if (counter > 100) return reject('not working');
       let progressContainer = document.querySelector('[class*=progress--progress-container]');
       let progressPrcnt = document.querySelector('[class*=progress-popover-content--progress-text]');
 
       if (!progressContainerLoadedAndClicked && progressContainer) {
-          document.querySelector("[class*=progress--progress-container]").click();
-          progressContainerLoadedAndClicked = true;
+        document.querySelector("[class*=progress--progress-container]").click();
+        progressContainerLoadedAndClicked = true;
       } else if (progressPrcnt) {
-          const courseTitle = document.querySelector("[class*=header--course-title]").textContent;
-          progressPrcnt = progressPrcnt.textContent;
-          progressPrcnt = progressPrcnt.split('of');
-          progressPrcnt[0] = progressPrcnt[0].trim();
-          progressPrcnt[1] = progressPrcnt[1].replace(' complete.', '').trim();
-          progressPrcnt = Math.round(progressPrcnt[0]/progressPrcnt[1]*1000)/10;
-          document.title = progressPrcnt + '% - ' + courseTitle;
-//             document.querySelector("[class*=header--course-title]").textContent = progressPrcnt + '% - ' + courseTitle;
-          document.querySelector("[data-purpose=progress-label]").textContent = 'Your progress: ' + progressPrcnt + '%';
-          document.querySelector("[class*=progress--progress-container]").click();
-          clearInterval(udemyStatInterval);
+        const courseTitle = document.querySelector("[class*=header--course-title]").textContent;
+        progressPrcnt = progressPrcnt.textContent;
+        progressPrcnt = progressPrcnt.split('of');
+        progressPrcnt[0] = progressPrcnt[0].trim();
+        progressPrcnt[1] = progressPrcnt[1].replace(' complete.', '').trim();
+        progressPrcnt = Math.round(progressPrcnt[0] / progressPrcnt[1] * 1000) / 10;
+        document.title = progressPrcnt + '% - ' + courseTitle;
+        document.querySelector("[data-purpose=progress-label]").textContent = 'Your progress: ' + progressPrcnt + '%';
+        document.querySelector("[class*=progress--progress-container]").click();
+        clearInterval(udemyStatInterval);
+        return resolve();
       }
 
-  }, 300);
+    }, 300);
+  })
 }
 
-(function() {
+(async function () {
   'use strict';
 
-  showProgressInTitle();
+  try {
+    await showProgressInTitle()
+    document.querySelector("[class*=progress--progress-circle]").addEventListener("click", (event) => {
+      event.preventDefault();
+      showProgressInTitle();
+    })
+  } catch (error) {
+    console.log(error);
+  }
 })();
 
