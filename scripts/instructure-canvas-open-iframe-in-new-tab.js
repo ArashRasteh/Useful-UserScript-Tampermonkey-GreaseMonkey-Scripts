6b2275c90c8303ef70bd4ca1737f44752f396ca6
ArashRasteh <ashrasteh@gmail.com>
2023-08-31 22:24:28 -0600

    Update instructure-canvas-open-iframe-in-new-tab.js
    Updated to be able to also use Alt+R to add the links, when page taking too long to loud.

--- scripts/instructure-canvas-open-iframe-in-new-tab.js
+++ scripts/instructure-canvas-open-iframe-in-new-tab.js
@@ -1,31 +1,68 @@
 // ==UserScript==
-// @name         Canvas Kaltura video link creator
+// @name         Instructure Kaltura video link creator
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        *.instructure.com/courses/*
 // @grant        none
 // ==/UserScript==
 
+var addNewTabLinksToIframesWithAllowFullScreen = function() {
+    let iframes = document.querySelectorAll('iframe[allowfullscreen]');
+    console.dir(iframes);
+
+    iframes.forEach((iframe, index) => {
+        let br = document.createElement("br");
+        let atag = document.createElement("a");
+        let iframeWrapper = iframe.parentNode;
+
+        atag.target = "_blank";
+        atag.innerText = 'Open in New Fullscreen Tab';
+        atag.href = iframe.src;
+        atag.style.display = 'block';
+        atag.style.textAlign = 'center';
+
+        //iframeWrapper.appendChild(br);
+        iframeWrapper.appendChild(atag);
+    });
+}
+
 (function() {
     'use strict';
+    addNewTabLinksToIframesWithAllowFullScreen();
 
     window.addEventListener('load', function () {
-        let iframes = document.querySelectorAll('iframe[allowfullscreen="allowfullscreen"]');
+        console.log('Instructure Kaltura video link creator Tampermonkey Script working');
 
-        iframes.forEach((iframe, index) => {
-            var br = document.createElement("br");
-            var atag = document.createElement("a");
-            var iframeWrapper = iframe.parentNode;
+        document.querySelectorAll('a[href*="kaltura"]').forEach(link => {
+            let atag = document.createElement("a");
+            let href = link.href;
+            let kalturaVideoID = href.substring(href.lastIndexOf('/') + 1);
 
             atag.target = "_blank";
-            atag.innerText = 'Open in New Tab';
-            atag.href = iframe.src;
+            atag.innerText = 'Open in New Fullscreen Tab';
+            atag.href = 'https://1533221.mediaspace.kaltura.com/embed/secure/iframe/entryId/' + kalturaVideoID;
+            atag.style.display = 'block';
+            atag.style.textAlign = 'center';
 
-            iframeWrapper.appendChild(br)
-            iframeWrapper.appendChild(atag)
+            link.append(atag);
         });
+
     })
 
 })();
+
+let keysPressed = {};
+
+window.addEventListener('keydown', function(event) {
+    keysPressed[event.key] = true;
+
+});
+
+window.addEventListener('keyup', function(event) {
+    if (keysPressed['Alt'] && (keysPressed['r'] || keysPressed['R'])) {
+        addNewTabLinksToIframesWithAllowFullScreen();
+    }
+    keysPressed = {};
+});
